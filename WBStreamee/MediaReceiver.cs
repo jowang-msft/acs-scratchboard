e64250d814e065b6942648aa330c2a9ea0b5a41f
Zheng Wang <jowang@microsoft.com>
2024-12-09 01:47:37 -0800

    Add WB audio receiver

---
+++ WBStreamee/MediaReceiver.cs
@@ -0,0 +1,76 @@
+﻿using NAudio.Wave;
+using System.Collections.Concurrent;
+
+// OpusDotNet doesn't do will with PCM -> Opus, so we'll use Concentus instead.
+// Concentus doesn't do well with Open -> PCM, so we'll use NAudio instead.
+
+namespace WBStreamee
+{
+    internal static class MediaReceiver
+    {
+        private static OpusDotNet.OpusDecoder decoder = new OpusDotNet.OpusDecoder(48000, 2);
+        private static WaveFormat waveFormat = new WaveFormat(48000, 16, 2);
+        private static BufferedWaveProvider bufferedWaveProvider;
+        private static WaveOutEvent waveOut = new WaveOutEvent();
+
+        private static ConcurrentQueue<byte[]> _audioQueue = new ConcurrentQueue<byte[]>();
+
+        internal static void Start()
+        {
+            bufferedWaveProvider = new BufferedWaveProvider(waveFormat)
+            {
+                BufferDuration = TimeSpan.FromSeconds(60),
+            };
+
+            waveOut.Init(bufferedWaveProvider);
+            waveOut.Play();
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (_audioQueue.TryDequeue(out byte[] media))
+                    {
+                        ProcessMedia(media);
+                    }
+                }
+            });
+        }
+
+        public static void Collect(byte[] media)
+        {
+            _audioQueue.Enqueue(media);
+        }
+
+        public static byte[] DecodeOpusToPcm(byte[] opusData, int sampleRate, int channels)
+        {
+            var opus = new ReadOnlySpan<byte>(opusData);
+
+            int frames = Concentus.Structs.OpusPacketInfo.GetNumFrames(opus);
+            int samplesPerFrame = Concentus.Structs.OpusPacketInfo.GetNumSamplesPerFrame(opus, 48000);
+            int packetDuration = frames * samplesPerFrame;
+
+            short[] pcmBuffer = new short[samplesPerFrame * channels];
+            byte[] pcmBytes = decoder.Decode(opusData, opusData.Length, out int decodedLength);
+
+            short[] pcmShorts = new short[decodedLength / 2];
+            Buffer.BlockCopy(pcmBytes, 0, pcmShorts, 0, decodedLength);
+
+            byte[] waveBuffer = new byte[pcmShorts.Length * sizeof(short)];
+            Buffer.BlockCopy(pcmShorts, 0, waveBuffer, 0, waveBuffer.Length);
+            
+            return waveBuffer;
+        }
+
+        internal static void ProcessMedia(byte[] media)
+        {
+            //var data = new ReadOnlySpan<byte>(media);
+
+            // Decode Opus data to PCM
+            byte[] pcmData = DecodeOpusToPcm(media, 48000, 2);
+            bufferedWaveProvider.AddSamples(pcmData, 0, pcmData.Length);
+
+            Console.WriteLine($"Processed media of size {pcmData.Length}");
+        }
+    }
+}
---
+++ WBStreamee/Program.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Net.Http.Json;
+using Waimea.Channel;
+using Waimea.Channel.Messages.Common;
+using Waimea.Channel.Messages.ToBackend;
+using Waimea.Channel.Messages.ToFrontend;
+
+namespace WBStreamee
+{
+    internal class Program
+    {
+        private const string WB_ORIGIN = "https://alphasandbox.dev.waimeabae.com";
+        private const uint AUDIO_FEED_VIEW_ID = 1;
+        private FrontendChannel channel = null;
+        private ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+        private FeedId roomFeedId = new FeedId() { Name = "room" };
+
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                var program = new Program();
+                await program.Connect("1800-go-away");
+            });
+
+            Console.WriteLine("Background task started!");
+
+            Console.ReadLine();
+        }
+
+        public async Task Connect(string roomId)
+        {
+            channel = new FrontendChannel(Str0mBackendFactory.Create, (message, attachment) => {
+                switch (message.MessageCase)
+                {
+                    case ToFrontendMessage.MessageOneofCase.ConnectionMetadata: {
+                            using (var client = new HttpClient())
+                            {
+                                var metadata = message.ConnectionMetadata;
+                                using (var content = JsonContent.Create(new {
+                                        metadata.EndpointId,
+                                        metadata.SecretToken,
+                                        profile = "videoconf",
+                                        roomId,
+                                    }))
+                                {
+                                    var result = client.PostAsync(new Uri(WB_ORIGIN + "/samples/app_server/activate"), content).Result;
+                                    Console.WriteLine($"Activation completed: {result.StatusCode}");
+                                }
+                            }
+
+                            MediaReceiver.Start();
+
+                            break;
+                        }
+                    case ToFrontendMessage.MessageOneofCase.ConnectionStateChange: {
+                            var stateChanged = message.ConnectionStateChange;
+                            switch (stateChanged.State)
+                            {
+                                case ConnectionState.Connected:
+                                    Console.WriteLine("Connected to WaimeaBay.");
+                                    break;
+                                case ConnectionState.Disconnected:
+                                    Console.WriteLine("Disconnected from WaimeaBay.");
+                                    break;
+                                default:
+                                    break;
+                            }
+
+                            break;
+                        }
+                    case ToFrontendMessage.MessageOneofCase.InboundMedia:
+                        {
+                            if (attachment != null)
+                            {
+                                var attachmentInBytes = attachment.ToBytes();
+
+                                // Using buffer
+                                MediaReceiver.Collect(attachmentInBytes);
+
+                                // No buffer
+                                // MediaReceiver.ProcessMedia(attachmentInBytes);
+
+                                attachment.Release();
+                            }
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }, loggerFactory.CreateLogger<FrontendChannel>());
+
+            channel.Send(new()
+            {
+                AddFeedView = new()
+                {
+                    FeedViewId = AUDIO_FEED_VIEW_ID,
+                    FeedId = roomFeedId,
+                    AudioConfig = new()
+                    {
+                        MaxStreams = 1,
+                        ReflectionMode = ReflectionMode.None,
+                    },
+                }
+            });
+
+            channel.Send(new()
+            {
+                AddOutboundMedia = new()
+                {
+                    SourceId = 1,
+                    AppEntityId = new AppEntityId("ACSCalling").ToByteString(),
+                    MediaType = new() { Audio = new() },
+                    AttachmentFormat = AttachmentFormat.Encoded,
+                    FeedIds = { roomFeedId }
+                }
+            });
+
+            channel.Send(new()
+            {
+                Connect = new()
+                {
+                    FrontdoorOrigin = WB_ORIGIN
+                }
+            });
+
+            Console.WriteLine("Waiting for Connect.");
+        }
+    }
+}
